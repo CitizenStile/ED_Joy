34b1c12d8cabfe221b685e58dfdbf99caf68d83c
CitizenStile <CitizenStile@pm.me>
2025-05-09 10:53:33 -0600

    Replacing os.path to pathlib.Path and adding logic to ensure that the path exists

--- ed_joy/__init__.py
+++ ed_joy/__init__.py
@@ -1,8 +1,16 @@
 import sys  # noqa: N999
-from os import path
+from pathlib import Path
 
 
-def resource_path(relative_path):
+def resource_path(relative_path, mkdir=False):
     """Get the absolute path to assets, works for both dev and PyInstaller"""
-    base_path = getattr(sys, '_MEIPASS', path.abspath("."))
-    return path.join(base_path, relative_path)
+    base_path = getattr(sys, '_MEIPASS', Path(".").resolve())
+    # base_path could be a str or Path, pass it to Path() for safety
+    ret_pth = Path(base_path).joinpath(relative_path)
+    if mkdir:
+        try:
+            ret_pth.parent.mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            print("Exception occured while creating parents path")
+            print(e)
+    return ret_pth
